8916f40587a1568b727b797e92acaeacd258f99d
Jonathan Souza <souzajonathan570@gmail.com>
2021-06-09 17:31:33 -0300

    criado a rota para a pagina carteira

---
+++ src/pages/Wallet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Wallet extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Wallet</h1>
+      </section>
+    );
+  }
+}
+
+export default Wallet;
